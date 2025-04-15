b22fa1af822ca751092da74a71619bd4b5c6d0d8
Manthan Bire <manthansharadbire@gmail.com>
2025-04-15 18:15:21 +0530

    Component & views created

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route,  Routes } from "react-router";
+import Homepage from './views/Homepage';
+import Movie from './views/Movie';
+import MovieId from './views/MovieId';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
---
+++ src/views/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Homepage() {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder='Movie name here'  />
+      </div>
+    </div>
+  )
+}
+
+export default Homepage;
