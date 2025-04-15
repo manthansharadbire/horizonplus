fc62c8958d889a1cbff298eac638a6b95c6397b7
Manthan Bire <manthansharadbire@gmail.com>
2025-04-15 17:48:53 +0530

    Routing setup throughout the views

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Route,  Routes } from "react-router";
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Homepage/>}/>
+    <Route path="/movie" element={<Movie/>}/>
+    <Route path="/movie/id" element={<MovieId/>}/>
+    </Routes>
+    </BrowserRouter>
 )
