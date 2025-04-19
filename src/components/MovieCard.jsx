312c5d7ef5d69d6e6efcfa419b26c078505af389
Manthan Bire <manthansharadbire@gmail.com>
2025-04-19 08:15:08 +0530

    Minor changes in card responsiveness

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
-function MovieCard({ Title, Poster, onClick }) {
+function MovieCard({  Poster, onClick }) {
   return (
     <div
-      className="flex flex-col items-center bg-white dark:bg-gray-900 rounded-lg overflow-hidden shadow-md hover:shadow-xl transform hover:scale-[1.03] transition-all duration-300 w-full max-w-[160px] sm:max-w-[180px] md:max-w-[200px] lg:max-w-[220px] xl:max-w-[240px] cursor-pointer"
+      className="flex flex-col items-center rounded-lg overflow-hidden shadow-md hover:shadow-xl transform hover:scale-[1.03] transition-all duration-300 w-full max-w-[160px] sm:max-w-[180px] md:max-w-[200px] lg:max-w-[220px] xl:max-w-[240px] cursor-pointer"
       onClick={onClick}
     >
-      <div className="w-full aspect-[2/3] bg-gray-200 dark:bg-gray-700">
+      <div className="w-full ">
         <img
           src={Poster}
-          alt={`${Title} Poster`}
-          className="w-full h-full object-cover"
+          alt={` Poster`}
+          className="w-full h-[220px] md:h-[300px] object-cover"
         />
       </div>
 
